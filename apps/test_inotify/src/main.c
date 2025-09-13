ea7ce82fa2ab13022866945b69638d9235b2ab2b
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2025-09-13 12:38:54 +0200

    update

--- apps/test_inotify/src/main.c
+++ apps/test_inotify/src/main.c
@@ -43,7 +43,8 @@ int main(int argc, char *argv[])
 	ECS_IMPORT(world, FlecsStats);
 	ECS_IMPORT(world, FlecsScriptMath);
 	ECS_IMPORT(world, EgFs);
-	
+	ECS_IMPORT(world, EgFsEpoll);
+	ECS_IMPORT(world, EgFsFanotify);
 
 	ecs_set(world, EcsWorld, EcsRest, {.port = 0});
 	printf("Remote: %s\n", "https://www.flecs.dev/explorer/?remote=true");
