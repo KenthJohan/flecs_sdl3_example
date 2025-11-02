7e6656e5c56e01d8622fb785bfaa5bdf4490ba2d
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2025-11-02 12:18:16 +0100

    update

--- apps/example_sdl3_flecs/src/shader_spirv.c
+++ apps/example_sdl3_flecs/src/shader_spirv.c
@@ -7,15 +7,12 @@
 #include <sys/wait.h>
 #include <SDL3/SDL_gpu.h>
 
-#include <egmisc/eg_popen.h>
+#include <ecsx.h>
 #include <egmisc/eg_file.h>
 
-
-
-static int static_glslangValidator(const char *namepart, const char *type, char const * ofile)
+static int static_glslangValidator(const char *namepart, const char *type, char const *ofile)
 {
 	char buffer[128];
-	// int len = snprintf(buffer, sizeof(buffer), "glslangValidator %s.%s.glsl -V -S %s -o %s.%s.spv --quiet", namepart, type, type, namepart, type);
 	int len = snprintf(buffer, sizeof(buffer), "glslangValidator %s.%s.glsl -V -S %s -o %s", namepart, type, type, ofile);
 	if (len < 0) {
 		ecs_err("sprintf error");
@@ -25,26 +22,21 @@ static int static_glslangValidator(const char *namepart, const char *type, char 
 		ecs_err("command buffer to small");
 		return -1;
 	}
-
 	ecs_strbuf_t str = ECS_STRBUF_INIT;
 	int r;
 	int exit_code = 0;
-	r = eg_popen(buffer, &exit_code, &str);
-
+	r = ecsx_os_popen_to_strbuf(buffer, &exit_code, &str);
 	char *strval = ecs_strbuf_get(&str);
-	if(strval) {
+	if (strval) {
 		fputs(strval, stdout);
 		ecs_os_free(strval);
 	} else {
 		ecs_err("ecs_strbuf_get() error");
 	}
-
-
 	return r;
 }
 
-
-static int static_spv_make_path(char * buffer, size_t buflen, const char *namepart, const char *type)
+static int static_spv_make_path(char *buffer, size_t buflen, const char *namepart, const char *type)
 {
 	int len = snprintf(buffer, buflen, "%s.%s.spv", namepart, type);
 	if (len < 0) {
@@ -58,10 +50,7 @@ static int static_spv_make_path(char * buffer, size_t buflen, const char *namepa
 	return len;
 }
 
-
-
-
-SDL_GPUShader * shader_spirv_compile(SDL_GPUDevice *device, const char *namepart, SDL_GPUShaderStage stage)
+SDL_GPUShader *shader_spirv_compile(SDL_GPUDevice *device, const char *namepart, SDL_GPUShaderStage stage)
 {
 	SDL_GPUShaderFormat format = SDL_GetGPUShaderFormats(device);
 	if (format != SDL_GPU_SHADERFORMAT_SPIRV) {
@@ -70,9 +59,8 @@ SDL_GPUShader * shader_spirv_compile(SDL_GPUDevice *device, const char *namepart
 	}
 
 	char ofilepath[128];
-	char const * type = NULL;
-	switch (stage)
-	{
+	char const *type = NULL;
+	switch (stage) {
 	case SDL_GPU_SHADERSTAGE_VERTEX:
 		type = "vert";
 		break;
@@ -109,8 +97,8 @@ SDL_GPUShader * shader_spirv_compile(SDL_GPUDevice *device, const char *namepart
 	createinfo.code_size = length;
 	createinfo.entrypoint = "main";
 	createinfo.stage = stage;
-	SDL_GPUShader * s = SDL_CreateGPUShader(device, &createinfo);
-	ecs_os_free((void*)alloc_code);
+	SDL_GPUShader *s = SDL_CreateGPUShader(device, &createinfo);
+	ecs_os_free((void *)alloc_code);
 
 	if (s == NULL) {
 		ecs_err("SDL_CreateGPUShader() failed");
@@ -118,10 +106,3 @@ SDL_GPUShader * shader_spirv_compile(SDL_GPUDevice *device, const char *namepart
 	}
 	return s;
 }
-
-
-
-
-
-
-
